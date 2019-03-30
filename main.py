681814ff150ce5ed4c570eb65e040f2f978cea15
scolle <colle.sebastien@neuf.fr>
2019-03-31 00:50:46 +0100

    Core : First Xml navigation in OpenXMLDoc schema.
    	extract a dict with all customProperties
    
    RAF: need
    	1 function to update au customProperty ( "by name", "value")
    	1 function to write new xml doc (or string)
    	1 function to flush new xml doc or string in zipped .docx document
    
    info : Write on Py3.6

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 #from zipfile import ZipFile as Zf
 import datetime
 import zipfile
+import re
+from xml.etree.ElementTree import ElementTree as ET, Element, SubElement, dump
+from xml.etree.ElementTree import fromstring as ETfromstring, tostring as ETtostring
 
 def print_content_file_info(archive_name):
     zf = zipfile.ZipFile(archive_name)
@@ -16,15 +19,29 @@ def print_content_file_info(archive_name):
         print ()
 
 def extract_zipped_content(archive_name, filename):
+    """ to document """
     zf = zipfile.ZipFile(archive_name)
     return zf.read(filename)
 
 def parse_customProp_file(xmlContent):
-    pass
-    
+    """ to document """
+    tree = ETfromstring(xmlContent)
+    childLst = tree.getchildren()
+    var = {}
+    for child in childLst:
+        var.update({child.get('name') : (child.getchildren())[0].text})
+    # navigation sample in tree
+    """
+    child = childLst[1]
+    print( child.get('name') )
+    print( (child.getchildren())[0].text)
+    """
+    return var
        
 if __name__ == '__main__':
     #print_content_file_info('REVD-2019.docx')
+    print ( 'exctact in rawD variable')
+    rawD = extract_zipped_content('REVD-2019.docx','docProps/custom.xml')
     print (
-        extract_zipped_content('REVD-2019.docx','docProps/custom.xml')
+        parse_customProp_file(rawD)
         )
